4051ba11520afd1d19329fbc601851b5e35bd711
Steve St.Pierre <stephencstpierre@gmail.com>
2023-03-22 12:16:11 -0400

    My Header solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <DesktopActionGroup position="end" direction="column" gap="8px">
+          <Button>SUBSCRIBE</Button>
+          <Link href="/">Already a subscriber?</Link>
+        </DesktopActionGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,40 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    justify-self: ${(p) => p.position || "start"};
+    flex-direction: ${(p) => p.direction || "row"};
+    align-items: center;
+    justify-content: flex-end;
+    gap: ${(p) => p.gap || "24px"};
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: revert;
+    justify-content: revert;
+  }
+`;
+
+const Link = styled.a`
+  text-decoration: underline;
+  font-weight: 400;
+  font-style: italic;
+  font-size: calc((14 / 16) * 1rem);
+  color: var(--color-gray-900);
 `;
 
 export default Header;
